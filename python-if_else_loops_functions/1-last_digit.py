e567d574010cbefe7a8eccd0ee760b40f717cf27
Isaac Rudasingwa <rudasaac@outlook.comssh-keygen -t rsa -c rudasaac@outlook.comgit config --global user.email rudasaac@outlook.com>
2022-07-03 02:45:16 -0700

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ if number < 0:
                 if last > 5:
                         print(f"Last digit of {number:d} is {last:d} and is greater than 5")
                     elif last == 0:
-                            print(f"Last digit of {number:d} is {last:d} and is 0")
+                            print("Last digit of {number:d} is {last:d} and is 0")
                         elif last < 6 and last != 0:
                                 print(f"Last digit of {number:d} is {last:d} and is less than 6 and not 0")
