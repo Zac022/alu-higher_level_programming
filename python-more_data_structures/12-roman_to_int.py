e36b56e59cff8cafe99e469c0c4d6be1c99013b8
Isaac Rudasingwa <rudasaac@outlook.comssh-keygen -t rsa -c rudasaac@outlook.comgit config --global user.email rudasaac@outlook.com>
2022-07-15 14:26:35 -0700

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
-
-
 def roman_to_int(roman_string):
     # check if the data type passed is not string
     if not isinstance(roman_string, str):
         return 0
     total = 0
     num = 0
-    digits = {'I': 1, 'V' : 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     # reversetheromannumeralstostartwiththetensofunitsandusearch unit
     for r in reversed(roman_string):
         num = digits[r]
