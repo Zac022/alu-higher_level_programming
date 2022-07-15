f2c7accdeaecf342612e0116cef6eff44be68998
Isaac Rudasingwa <rudasaac@outlook.comssh-keygen -t rsa -c rudasaac@outlook.comgit config --global user.email rudasaac@outlook.com>
2022-07-14 22:27:38 -0700

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,32 +2,14 @@
 
 
 def roman_to_int(roman_string):
-    final_value = 0
-    counter = 0
-    if isinstance(roman_string, str) == False or roman_string is None:
-        return final_value
-    else:
-        for i in roman_string:
-            if i == "M":
-                final_value += 1000
-                counter += 1
-            elif i == "D":
-                final_value += 500
-                counter += 1
-            elif i == "C":
-                final_value += 100
-                counter += 1
-            elif i == "L":
-                final_value += 50
-                counter += 1
-            elif i == "X":
-                final_value += 10
-                counter += 1
-            elif i == "V":
-                final_value += 5
-                counter += 1
-            elif i == "I" and counter == 0:
-                final_value += 1
-            elif i == "I" and counter > 0:
-                final_value -= 1
-        return final_value
+        # check if the data type passed is not string
+            if not isinstance(roman_string, str):
+                        return 0
+                        total = 0
+                            num = 0
+                                digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+                                    # reveresetheromannumeralstostartwiththetensofunitsanduseeach unit
+                                        for r in reversed(roman_string):
+                                                    num = digits[r]
+                                                            total += num if total < num * 5 else -num
+                                                                return total
