ba9568c65e4e032a509b42ed7387fbe448f34add
Isaac Rudasingwa <rudasaac@outlook.comssh-keygen -t rsa -c rudasaac@outlook.comgit config --global user.email rudasaac@outlook.com>
2022-08-05 01:36:34 -0700

    ..?

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,12 +1,7 @@
--- List all cities of CA that can be found in db 'hbtn_0d_usa'
--- 'states' table contains only one record where 'name = California'
--- but 'id' cant be different
--- Results must be sorted in ascending order by 'cities.id'
--- Not allowed to use JOIN keyword
-SELECT id, name
-FROM cities
-WHERE state_id = (
-      SELECT id
-      FROM states
-      WHERE name = 'California'
-);
+-- List all cities in db 'hbtn_0d_usa'
+-- Each record should display cities.id, cities.name, and states.name
+-- Can only use SELECT statement once
+SELECT cities.id, cities.name, states.name
+FROM states
+INNER JOIN cities
+ON states.id = cities.state_id;
