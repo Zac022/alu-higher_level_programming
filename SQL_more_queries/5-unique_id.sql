acebc877fac96b07dc8664fa016b43bcf6cf826e
Isaac Rudasingwa <rudasaac@outlook.comssh-keygen -t rsa -c rudasaac@outlook.comgit config --global user.email rudasaac@outlook.com>
2022-08-05 01:32:01 -0700

    ..?

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- Create table 'unique_id'
+-- id INT default value 1, name VARCHAR(256)
+-- If table already exists, script should not fail
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1, name VARCHAR(256));
