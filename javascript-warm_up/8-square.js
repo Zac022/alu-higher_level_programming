06cf2058c013f7a641cff9cd40a4c13f379954c0
Isaac <rudasaacv@gmail.com>
2022-10-07 11:18:43 +0200

    8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,9 +5,9 @@ let output;
 const arg = process.argv;
 const cnv = Number(arg[2]);
 if (cnv) {
-  for (let a = 0; a < cnv; a++) {
+  for (let i = 0; i < cnv; i++) {
     output = '';
-    for (let b = 0; b < cnv; b++) {
+    for (let j = 0; j < cnv; j++) {
       output += 'X';
     }
     console.log(output);
