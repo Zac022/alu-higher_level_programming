38d41e36075d76ce8ea9d203a3cee42d730ffd28
Isaac <rudasaacv@gmail.com>
2022-10-07 11:57:14 +0200

    11-second_biggest.js

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const arg = process.argv;
+const con1 = Number(arg[2]);
+const con2 = Number(arg[3]);
+let bigSoFar = con1;
+let secondlargest = con2;
+if (args.length <= 3) {
+  console.log(0);
+} else {
+  for (let i = 3; i < args.length; i++) {
+    if (Number(arg[i]) > bigSoFar) {
+      secondlargest = bigSoFar;
+      bigSoFar = Number(arg[i]);
+    }
+    if (Number(arg[i]) > secondlargest && Number(arg[i]) < bigSoFar) {
+      secondlargest = Number(arg[i]);
+    }
+  }
+  console.log(secondlargest);
+}
