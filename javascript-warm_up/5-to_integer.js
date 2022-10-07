4034c1db426cfcaa75cb02161514f186eaa1fa21
Isaac <rudasaacv@gmail.com>
2022-10-07 10:57:52 +0200

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const arg = process.argv;
-const convs = Number(args[2]);
-if (convs) {
-  console.log(`the number: ${arg[2]}`);
+const cnv = Number(arg[2]);
+if (cnv) {
+  console.log(`My number: ${arg[2]}`);
 } else {
   console.log('Not a number');
 }
