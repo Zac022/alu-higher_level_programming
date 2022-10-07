478291b7d5a49662166509774f2bb5db047bb0d6
Isaac <rudasaacv@gmail.com>
2022-10-07 11:14:38 +0200

    8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+
+
+#!/usr/bin/node
+let output;
+const arg = process.argv;
+const cnv = Number(args[2]);
+if (cnv) {
+  for (let a = 0; a < cnv; a++) {
+    output = '';
+    for (let b = 0; b < cnv; b++) {
+      output += 'X';
+    }
+    console.log(output);
+  }
+} else {
+  console.log('Missing size');
+}
